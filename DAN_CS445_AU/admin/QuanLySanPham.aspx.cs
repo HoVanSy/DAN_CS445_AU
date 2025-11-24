971bbbf83cedc9ed55ed3d78f79cb7db3b7c0822
TRung <lamquangtrung75@gmail.com>
2025-11-24 22:29:42 +0700

    Giao dien QL San Pham

---
+++ DAN_CS445_AU/admin/QuanLySanPham.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DAN_CS445_AU.admin
+{
+    public partial class QuanLySanPham : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
